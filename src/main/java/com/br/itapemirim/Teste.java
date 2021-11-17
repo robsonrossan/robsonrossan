5f9b0005c19fc42bf8dc0e79e40a4e14fe013b88
Robson dos Santos <robsonrossan@hotmail.com>
2021-11-17 18:56:15 -0300

    first commit

--- src/main/java/com/br/itapemirim/Teste.java
+++ src/main/java/com/br/itapemirim/Teste.java
@@ -7,7 +7,7 @@ public class Teste {
 	public static void main(String[] args) {
 
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		System.out.println(passwordEncoder.encode("123"));
+		System.out.println(passwordEncoder.encode("123")); 
 	}
 
 }
